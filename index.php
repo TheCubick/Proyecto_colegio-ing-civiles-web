580b307f1cbf7ec91f735920b481a09a8fe08df6
Pablo <pabloarg120@gmail.com>
2026-09-02 22:28:32 -0600

    ADD: carpetas estructuradas y archivo index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+    // titulo variable
+    $titulo_pag = "Inicio | Pantalla principal";
+    // se incluye el encabezado
+    include 'html/header.html';
+?>
+<!-- Se cierra el segmento de php para poder escribir el código html -->
+ 
+    <body>
+        <p>Esta será la pantalla principal donde trabajaremos</p>
+    </body>
